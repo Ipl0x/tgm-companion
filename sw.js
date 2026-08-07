bde59ea52d7868c00d51f238f13823bf5affc475
Ipl0x <ipl0x@outlook.com>
2026-08-07 17:57:10 +0200

    chore: refresh PWA cache for Basic Resource data

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'tgm-companion';
-const CACHE_VERSION = '2026-08-07-v12';
+const CACHE_VERSION = '2026-08-07-v13';
 const APP_CACHE = `${CACHE_PREFIX}-${CACHE_VERSION}`;
 
 const APP_SHELL = [
